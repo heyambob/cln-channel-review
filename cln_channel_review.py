64dd27edcf3ec9114205647603a7f66e4304b0d6
k <k@ks-MacBook.local>
2022-04-11 21:43:01 +0700

    supported multiple xdays

--- cln_channel_review.py
+++ cln_channel_review.py
@@ -14,7 +14,7 @@ parser = argparse.ArgumentParser(description="c-lightning channel review",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--cli", default="lightning-cli", help="your lightning-cli command")
 parser.add_argument("--cli-args", default=[], nargs='+', help="lightning-cli arguments ommitting --, for example --lightning-dir would be lightning-dir")
-parser.add_argument("--xdays", default=30, type=int,help="last forward in xdays")
+parser.add_argument("--xdays", nargs="*", default=[7,30], type=int,help="last forward in xdays(can be list)")
 parser.add_argument("--peer-id", help="peer pubkey that you want to review otherwise it will review all your peers")
 parser.add_argument("--recent-forward", help="review peers that forwards from i to j days")
 parser.add_argument("--absent-forward", default=-1, type=int, help="review peers that have no forwards in the last n days")
@@ -121,26 +121,28 @@ for peer in all_peers:
       channel_balance = peer["channels"][0]["msatoshi_to_us"]
       ratio = channel_balance/channel_size
       
-      num_in_forward_last_xdays=0
-      num_out_forward_last_xdays=0
-      msat_earn_last_xdays=0
-      ppm_out_last_xdays=[]
+      num_in_forward_last_xdays = list(map(lambda d: 0, xdays))
+      num_out_forward_last_xdays = list(map(lambda d: 0, xdays))
+      msat_earn_last_xdays = list(map(lambda d: 0, xdays))
+      ppm_out_last_xdays = list(map(lambda d: [], xdays))
+      in_xdays_forward = list(map(lambda d: 0, xdays))
+      out_xdays_forward = list(map(lambda d: 0, xdays))
+
       last_in_forward=0
       last_out_forward=0
       last_ppm=0
       in_total_forward=0
       out_total_forward=0
-      in_xdays_forward=0
-      out_xdays_forward=0
 
       ct=int(time.time())
 
       for infw in call_rpc("listforwards","-k","status=settled","in_channel="+peer["channels"][0]["short_channel_id"])["forwards"]:
         ts = int(infw["resolved_time"])
 
-        if ct-ts < 86400*xdays:
-          num_in_forward_last_xdays += 1
-          in_xdays_forward += int(infw["in_msat"][:-4])
+        for idx, xday in enumerate(xdays):
+          if ct-ts < 86400*xday:
+            num_in_forward_last_xdays[idx] += 1
+            in_xdays_forward[idx] += int(infw["in_msat"][:-4])
 
         in_total_forward += int(infw["in_msat"][:-4])
         last_in_forward = max( int(infw.get("resolved_time", 0)), last_in_forward)
@@ -157,11 +159,12 @@ for peer in all_peers:
         if ts > last_out_forward:
           last_out_forward = ts
 
-        if ct-ts < 86400*xdays:
-          num_out_forward_last_xdays += 1
-          msat_earn_last_xdays += outfw["fee"]
-          ppm_out_last_xdays += [ppm]
-          out_xdays_forward += int(outfw["out_msat"][:-4])
+        for idx, xday in enumerate(xdays):
+          if ct-ts < 86400*xday:
+            num_out_forward_last_xdays[idx] += 1
+            msat_earn_last_xdays[idx] += outfw["fee"]
+            ppm_out_last_xdays[idx] += [ppm]
+            out_xdays_forward[idx] += int(outfw["out_msat"][:-4])
 
       colored_alias=colored(peerinfo["alias"],'green' if peer["connected"] else 'red')
       colored_ratio=colored("%.2f"%(ratio),('red' if ratio <= 0.2 else 'yellow' if ratio >= 0.8 else 'green'))
@@ -174,19 +177,22 @@ for peer in all_peers:
       colored_in_forward_days_ago=colored("%d"%(in_forward_days_ago),'green' if in_forward_days_ago <= 7 else 'yellow' if in_forward_days_ago <= 20 else 'red')
       colored_out_forward_days_ago=colored("%d"%(out_forward_days_ago),'green' if out_forward_days_ago <= 7 else 'yellow' if out_forward_days_ago <= 20 else 'red')
 
-      colored_num_in_forward_xdays=colored("%d"%(num_in_forward_last_xdays),'green' if num_in_forward_last_xdays >= 5 else 'yellow' if num_in_forward_last_xdays >= 2 else 'red')
-      colored_num_out_forward_xdays=colored("%d"%(num_out_forward_last_xdays),'green' if num_out_forward_last_xdays >= 5 else 'yellow' if num_out_forward_last_xdays >=2 else 'red')
-
+      num_in_out_format = lambda x: colored("%d"%(x),'green' if x >= 5 else 'yellow' if x >= 2 else 'red')
+      colored_num_in_forward_xdays = list(map(num_in_out_format, num_in_forward_last_xdays))
+      colored_num_out_forward_xdays = list(map(num_in_out_format, num_out_forward_last_xdays))
 
       print("%s(%s) - %d out of %d"%(peer["id"],colored_alias,progress,len(all_peers)))
       print("channel size: %.2fM, to_us %.4fM, ratio %s"%(channel_size/1000000000,channel_balance/1000000000,colored_ratio))
       print("local_fee%s remote_fee(%d,%d)"%(colored_local_fee,remote_fee_base,remote_fee_ppm))
       print("last ppm %s, in forward %s days ago, out forward %s days ago"%(colored_last_ppm, colored_in_forward_days_ago, colored_out_forward_days_ago))
-      print("Msat in/out forwards (%.2f,%.2f) ; %s days ago (%.2f,%.2f)"%(in_total_forward/ONE_M,out_total_forward/ONE_M,xdays,in_xdays_forward/ONE_M,out_xdays_forward/ONE_M))
-      print("last %s days num_forward(in %s, out %s)"%(xdays,colored_num_in_forward_xdays,colored_num_out_forward_xdays))
-      print("last %d days fee earned %d"%(xdays,msat_earn_last_xdays/1000))
-      if len(ppm_out_last_xdays) > 0:
-        print("last %d days ppm min %d, avg %d, median %d, max %d"%(xdays,min(ppm_out_last_xdays),mean(ppm_out_last_xdays),median(ppm_out_last_xdays),max(ppm_out_last_xdays)))
+      print("Msat in/out forwards (%.2f,%.2f)"%(in_total_forward/ONE_M,out_total_forward/ONE_M))
+      for idx, xday in enumerate(xdays):
+        print("Msat in/out forwards %s days ago (%.2f,%.2f)"%(xday,in_xdays_forward[idx]/ONE_M,out_xdays_forward[idx]/ONE_M))
+        print("last %s days num_forward(in %s, out %s)"%(xday,colored_num_in_forward_xdays[idx],colored_num_out_forward_xdays[idx]))
+        print("last %d days fee earned %d"%(xday,msat_earn_last_xdays[idx]/1000))
+        if len(ppm_out_last_xdays[idx]) > 0:
+          print("last %d days ppm min %d, avg %d, median %d, max %d"%(
+            xday,min(ppm_out_last_xdays[idx]),mean(ppm_out_last_xdays[idx]),median(ppm_out_last_xdays[idx]),max(ppm_out_last_xdays[idx])))
 
       peer_fees = peer_s_remote_ppms(peer["id"])
 
